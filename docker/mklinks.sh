2b4421b9d44469c0fea479c2f38fd229740aa1d2
alandoyle <me@alandoyle.com>
2023-10-18 00:32:35 +0100

    More Docker tweaks.

---
+++ docker/mklinks.sh
@@ -0,0 +1,7 @@
+#!/bin/sh -e
+#
+# lorg - mklinks.sh
+#
+
+ln -sf /usr/share/lorg/app /var/www/lorg/app
+ln -sf /usr/share/lorg/public /var/www/lorg/public
--- docker/startup.sh
+++ docker/startup.sh
@@ -5,30 +5,34 @@
 
 
 DST_DIR=/var/www/lorg
+GIT_DIR=/usr/share/lorg
 SRC_REPO=https://github.com/alandoyle/lorg
 
 # Create the 'app' user
 if ! id app >/dev/null 2>&1; then
 	addgroup --gid $OWNER_GID app
-	useradd -d /var/www/lorg -g app -u $OWNER_UID app
-   usermod -aG www-data,app apps
+	useradd -d /var/www -g app -u $OWNER_UID app
+    usermod -aG www-data,app apps
 fi
 
-if [ ! -d $DST_DIR/.git ]; then
-	mkdir -p $DST_DIR
-	chown $OWNER_UID:$OWNER_GID $DST_DIR
-
-	echo cloning lorg source from $SRC_REPO to $DST_DIR...
-	sudo -u app git clone $SRC_REPO $DST_DIR || echo error: failed to clone master repository.
-else
-	echo updating lorg source in $DST_DIR from $SRC_REPO...
-
+if [ ! -d $GIT_DIR/.git ]; then
+	[ ! -d $DST_DIR ] && mkdir -p $DST_DIR
 	chown -R $OWNER_UID:$OWNER_GID $DST_DIR
-	cd $DST_DIR && \
+
+	echo cloning lorg source from $SRC_REPO to $GIT_DIR...
+	sudo -u app git clone $SRC_REPO $GIT_DIR || echo error: failed to clone repository.
+else
+	echo updating lorg source in $GIT_DIR from $SRC_REPO...
+
+	chown -R $OWNER_UID:$OWNER_GID $GIT_DIR
+	cd $GIT_DIR && \
 		sudo -u app git config core.filemode false && \
 		sudo -u app git config pull.rebase false && \
-		sudo -u app git pull origin master || echo error: unable to update master repository.
+		sudo -u app git pull || echo error: unable to update repository.
 fi
+
+chmod a+x $GIT_DIR/docker/mklinks.sh
+$GIT_DIR/docker/mklinks.sh
 
 update-ca-certificates || true
 
@@ -39,7 +43,7 @@ if [ ! -e $DST_DIR/public/index.php ]; then
 fi
 
 if [ ! -f ${DST_DIR}/config/config.php ] ; then
-    cp ${DST_DIR}/docker/config.php ${DST_DIR}/config/config.php
+    cp ${GIT_DIR}/docker/config.php ${DST_DIR}/config/config.php
 fi
 chmod 644 $DST_DIR/config/config.php
 
@@ -56,8 +60,8 @@ rm -f /tmp/error.log && mkfifo /tmp/error.log && chown app:app /tmp/error.log
 
 (tail -q -f /tmp/error.log >> /proc/1/fd/2) &
 
-if [ -d ${DST_DIR}/template.original ] ; then
-    cp -Rf ${DST_DIR}/template.original/* ${DST_DIR}/template
+if [ -d ${GIT_DIR}/template ] ; then
+    cp -Rf ${GIT_DIR}/template/* ${DST_DIR}/template
 fi
 
 touch $DST_DIR/.app_is_ready
